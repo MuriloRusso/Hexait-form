176fb9b42206bbe701a25a91c43fa73595c17338
Murilo Russo <contato@murilorusso.com.br>
2023-07-04 18:07:21 -0300

    Adicionado Disparo de e-mail

--- config.php
+++ config.php
@@ -37,11 +37,11 @@
 
 	$sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL" . $mysqli->error);		
 
-	header("Location: index.html");
+	// header("Location: index.html");
 
 	//---------------------
 
-/*
+
 	require 'PHPMailer-master/PHPMailerAutoload.php';
 
 	$mail = new PHPMailer;
@@ -57,76 +57,39 @@
 
 	$mail->setFrom($mail->Username, 'Hexait'); //remetente
 
-	$mail->addAddress('juridico@hexait.com.br');
-//	$mail->addAddress('contato@murilorusso.com.br');
+	// $mail->addAddress('juridico@hexait.com.br');
+	$mail->addAddress('contato@murilorusso.com.br');
 
-	$mail->addAttachment('./form-config/file/'.$novoNomeDoArquivo.'.'.$extensao, $nomeOriginal.'.'.$extensao);
-
-
-	$mail->Subject = 'Formulário de Denúncia';
+	$mail->Subject = 'Agendamento Reunião';
 
 	$mail->Subject = '=?UTF-8?B?'.base64_encode($mail->Subject).'?=';
 
 
-	if($nomes === 'sim'){
+	$conteudo_email = " 
+	
+	<strong>Nome: </strong><br> $nome <br> <br>
 
-		$conteudo_email = " 
+	<strong>Sobrenome: </strong><br> $sobrenome <br> <br>
+	
+	<strong>E-mail: </strong><br> $email <br> <br>
 
-			<strong>Gostaria de se identificar?</strong><br> $nomes <br> <br>
-			
-			<strong>Nome: </strong><br> $nome <br> <br>
-			
-			<strong>E-mail: </strong><br> $email <br> <br>
-			
-			<strong>Telefone: </strong><br> $telefone <br> <br>
+	<strong>Organização:</strong> <br> $organizacao  <br> <br>
 
-		   <strong>	Tipo de Denúncia.</strong> <br> $denuncia  <br> <br>
+	<strong>Pais:</strong> <br> $pais  <br> <br>
 
-			<strong>Por favor, forneça uma descrição minuciosa do incidente, incluindo detalhes adicionais, como outros acontecimentos relevantes, pessoas envolvidas e informações que possam ter sido divulgadas indevidamente.</strong> <br> $descricaoIncidente <br> <br>
+	<strong>Cargo:</strong> <br> $cargo  <br> <br>
 
-			<strong>Qual é o seu grau de relacionamento com a HexaIT?</strong> <br> $relacionamento <br> <br>
+	<strong>EmployeeSize:</strong> <br> $EmployeeSize  <br> <br>
 
-			<strong>Descrição detalhada da denúncia.</strong> <br> $descricaoDenuncia <br> <br>
+	<strong>Telefone: </strong><br> $telefone <br> <br>
 
-
-			<strong>Anexos de Evidência.</strong> <br>
-
-			<a href='https://www.hexait.com.br/form-config/file/".$novoNomeDoArquivo.".".$extensao."'>Abrir</a>
+	<strong>tema:/strong> <br> $tema  <br> <br>
 
 
-			<br><br><br>
-			Este e-mail foi enviado em $data_envio às $hora_envio <br>
+	<br><br><br>
+	Este e-mail foi enviado em $data_envio às $hora_envio <br>
 
-		  ";
-		
-	}
-
-	else {
-		
-		$conteudo_email = " 
-
-			<strong>Gostaria de se identificar?</strong><br> $nomes <br> <br>
-
-		   <strong>	Tipo de Denúncia.</strong> <br> $denuncia  <br> <br>
-
-			<strong>Por favor, forneça uma descrição minuciosa do incidente, incluindo detalhes adicionais, como outros acontecimentos relevantes, pessoas envolvidas e informações que possam ter sido divulgadas indevidamente.</strong> <br> $descricaoIncidente <br> <br>
-
-			<strong>Qual é o seu grau de relacionamento com a HexaIT?</strong> <br> $relacionamento <br> <br>
-
-			<strong>Descrição detalhada da denúncia.</strong> <br> $descricaoDenuncia <br> <br>
-
-
-			<strong>Anexos de Evidência.</strong> <br>
-
-			<a href='https://www.hexait.com.br/form-config/file/".$novoNomeDoArquivo.".".$extensao."'>Abrir</a>
-
-
-			<br><br><br>
-			Este e-mail foi enviado em $data_envio às $hora_envio <br>
-
-		  ";
-		
-	}
+	";
 
 
 	$conteudo_email = utf8_decode($conteudo_email);
@@ -139,26 +102,26 @@
 
 	if($mail->send()){
 		
-		$mail->ClearAllRecipients();
+// 		$mail->ClearAllRecipients();
 		
-		$mail->addAddress('diretoria@hexait.com.br');
-//		$mail->addAddress('murilo@2up.com.br');
+// 		// $mail->addAddress('diretoria@hexait.com.br');
+// //		$mail->addAddress('murilo@2up.com.br');
 		
-		$mail->send();
+// 		$mail->send();
 		
 		
 		
-		header("Location: ../index.php?alert=sucess");
+		header("Location: index.html?alert=sucess");
 		
 	}
 
 	else{
 		
-		header("Location: index.php?alert=error".$mail->ErrorInfo);
+		header("Location: index.html?alert=error".$mail->ErrorInfo);
 		
 	}
 
-*/
+
 
 
 ?>
